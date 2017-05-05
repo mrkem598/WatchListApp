d9803ce10894f55ca58bd62430fdbbd633e7f61e
Moh <mohakemal9@gmail.com>
2017-05-05 00:48:39 -0400

    Update server.js

--- server.js
+++ server.js
@@ -1,1 +1,8 @@
-﻿
+//dependencies
+var express = require("express");
+var bodyParser = require("bodyParser");
+var methodOverride = require("method-override");
+//creating express app with a given port
+var app = express();
+var port = 3050;
+// Server static content for the app from the "public" directory in the application directory.
