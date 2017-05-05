2d2331f2ebe3e1c8095c1de9dd5c0a1437dd5563
lubilove2 <lubaba_yemer@yahoo.com>
2017-05-05 06:33:33 -0400

    mysql schema updated

---
+++ schema.sql
@@ -0,0 +1,13 @@
+CREATE DATABASE movie_planner_db;
+USE movie_planner_db;
+
+-- Create the table plans.
+CREATE TABLE movies
+(
+id int NOT NULL AUTO_INCREMENT,
+movie varchar(255) NOT NULL,
+PRIMARY KEY (id)
+);
+
+-- Insert a set of records.
+INSERT INTO movies (movie) VALUES ('Rush Hour 2');
